8ff5c084922322ab83dab9052302563f3b308abc
Palak <palakg2210@gmail.com>
2026-08-03 22:04:40 +0530

    Day 1: AddNumber

---
+++ src/day001/addNumbers.java
@@ -0,0 +1,11 @@
+package day001;
+
+public class addNumbers {
+    public static void main(String[] args)
+    {
+        int a=8;
+        int b=7;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
