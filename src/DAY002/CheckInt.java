78342906981ecd188a48f0594ae86717e26954af
Palak <palakg2210@gmail.com>
2026-08-05 22:20:37 +0530

    Day 2: literals&userInput

---
+++ src/DAY002/CheckInt.java
@@ -0,0 +1,12 @@
+package DAY002;
+
+import java.util.Scanner;
+
+public class CheckInt {
+    public static void main(String[] args){
+        System.out.println("Enter the number");
+        Scanner sc=new Scanner(System.in);
+        boolean b1 =sc.hasNextInt();
+        System.out.println(b1);
+    }
+}
