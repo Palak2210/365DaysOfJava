65bb8d36a090063d08387454dc987c0edb7528dd
Palak <palakg2210@gmail.com>
2026-08-05 05:45:17 +0530

    Day 2: literals&userInput

---
+++ src/DAY002/percentage.java
@@ -0,0 +1,26 @@
+package DAY002;
+import java.util.Scanner;
+public class percentage {
+    public static void main(String[] args){
+        int total=500;
+        System.out.println("Total of 5 subject is 500");
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter Maths number:");
+        int maths =a.nextInt();
+        Scanner b = new Scanner(System.in);
+        System.out.println("Enter English number:");
+        int english=b.nextInt();
+        Scanner c = new Scanner(System.in);
+        System.out.println("Enter Hindi number:");
+        int hindi=c.nextInt();
+        Scanner d = new Scanner(System.in);
+        System.out.println("Enter Science number:");
+        int science=d.nextInt();
+        Scanner e = new Scanner(System.in);
+        System.out.println("Enter Social.Studies number:");
+        int social_studies=e.nextInt();
+        int total_marks=maths+english+hindi+science+social_studies;
+        double percentage=total_marks*100/total;
+        System.out.println("Percentage of all five subjects are: " + percentage);
+    }
+}
