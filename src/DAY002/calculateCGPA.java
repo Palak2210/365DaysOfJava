48f4ee2412d50bb51f197f950dce44cbe03da4d6
Palak <palakg2210@gmail.com>
2026-08-05 21:58:52 +0530

    Day 2: literals&userInput

--- src/DAY002/calculateCGPA.java
+++ src/DAY002/calculateCGPA.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class calculateCGPA {
     public static void main(String[] args){
-        System.out.println("Enter the first number");
+        System.out.println("Enter the subject1 number");
     Scanner a = new Scanner(System.in);
     int num1 = a.nextInt();
-        System.out.println("Enter the second number");
+        System.out.println("Enter the subject2 number");
     Scanner b = new Scanner(System.in);
     int num2 = b.nextInt();
-        System.out.println("Enter the third number");
+        System.out.println("Enter the subject3 number");
     Scanner c = new Scanner(System.in);
     int num3 = c.nextInt();
-    int sum=num1+num2+num3;
+    double sum=num1+num2+num3;
         System.out.println("Sum of three numbers: " + sum);
         int total=300;
         double percentage=(sum*100)/total;
