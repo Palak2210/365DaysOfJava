87426ab9541a3a2f5b12a892e5810664f27fe398
Palak <palakg2210@gmail.com>
2026-08-06 20:56:35 +0530

    Day 3: Operators and expression

---
+++ src/day003/expression.java
@@ -0,0 +1,11 @@
+package day003;
+
+public class expression {
+    public static void main(String[] args){
+        float a = 7/2 * 9/2;
+        float b = 7/2 * (9/2);
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
