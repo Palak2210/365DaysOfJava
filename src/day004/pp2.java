b7816ee0be28369bdfaf2c49ec6884b35cc1a9b0
Palak <palakg2210@gmail.com>
2026-08-08 23:13:15 +0530

    Day 4: String

---
+++ src/day004/pp2.java
@@ -0,0 +1,9 @@
+package day004;
+
+public class pp2 {
+    public static void main(String[] args){
+        String text="Palak Gupta";
+        String b = text.replace(" ","_");
+        System.out.println(b);
+    }
+}
