c8abe2c349c3cfd2aa90e3f94da30488a97461e0
Palak <palakg2210@gmail.com>
2026-08-08 23:50:15 +0530

    Day 4: String

---
+++ src/day004/pp4.java
@@ -0,0 +1,9 @@
+package day004;
+
+public class pp4 {
+    public static void main(String[] args){
+        String name="This string contain    double and    triple  spaces.";
+        System.out.println(name.indexOf("  "));
+        System.out.println(name.indexOf("   "));
+    }
+}
