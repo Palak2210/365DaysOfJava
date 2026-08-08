55917a36a5083410b357f74c5da36a2f712dfb64
Palak <palakg2210@gmail.com>
2026-08-08 21:35:23 +0530

    Day 4: String

---
+++ src/day004/print.java
@@ -0,0 +1,11 @@
+package day004;
+
+public class print {
+    public static void main(String[] args){
+        int a=4;
+        float b=4.567f;
+        char c='P';
+        String d ="Palak";
+        System.out.printf("the value of a is %d , value of b is %f , value of c is %c and the value of d is %s",a,b,c,d);
+    }
+}
