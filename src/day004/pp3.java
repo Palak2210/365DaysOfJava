3467f39bc9481c9bb4efe45822ac59b3d18ce8a7
Palak <palakg2210@gmail.com>
2026-08-08 23:19:52 +0530

    Day 4: String

---
+++ src/day004/pp3.java
@@ -0,0 +1,9 @@
+package day004;
+
+public class pp3 {
+    public static void main(String[] args){
+        String name="Dear name,Thanks a lot";
+        name = name.replaceAll("name","Palak");
+        System.out.println(name);
+    }
+}
