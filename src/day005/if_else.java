c368d087afe3fda6395193b62e31680b7ea7ac35
Palak <palakg2210@gmail.com>
2026-08-09 10:28:15 +0530

    Day 5: Conditions

---
+++ src/day005/if_else.java
@@ -0,0 +1,13 @@
+package day005;
+
+public class if_else {
+    public static void main(String[] args){
+        int age=18;
+        if(age>=18){
+            System.out.println("you can drive");
+        }
+        else{
+            System.out.println("You cannot drive");
+        }
+    }
+}
