b7afac77a069dbd7ac793bae1b1127d98bd68ee1
Palak <palakg2210@gmail.com>
2026-08-09 12:37:35 +0530

    Day 6: Switch

---
+++ src/day006/switch_case.java
@@ -0,0 +1,25 @@
+package day006;
+import java.util.Scanner;
+public class switch_case {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age: ");
+        int age = sc.nextInt();
+        switch (age){
+            case 14:
+                System.out.println("You are Child");
+                break;
+            case 30:
+                System.out.println("You are adult");
+                break;
+            case 56:
+                System.out.println("You are Senior");
+                break;
+            case 75:
+                System.out.println("You are Senior-Citizen");
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+    }
+}
